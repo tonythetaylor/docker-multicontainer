1c96db801b89fdec14ff7c6e40aae339f405b357
Anthony Taylor <ant.taylor2@gmail.com>
2024-01-31 01:05:22 -0500

    updates

--- client/src/components/screens/detail/Details.js
+++ client/src/components/screens/detail/Details.js
@@ -1,30 +1,35 @@
 import React from "react";
-import { useCallback, useState, useEffect } from "react";
-import { useLocation, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import axios from "axios";
 import "./Details.css";
 const Details = () => {
   const { state } = useLocation();
   const [data, setData] = useState([]);
 
-  const getNumberById = useCallback(async (id) => {
-    // we will use nginx to redirect it to the proper URL
-    const dataByIdResponse = await axios.get(`/api/values/${id}`);
-    setData(dataByIdResponse.data.map((row) => row));
-      // console.log(`\x1b[33m DEBUG : ---> ${data.map((value) => value.username)} \x1b[0m`)
-  }, []);
+  // const getNumberById = useCallback(async (id) => {
+  //   // we will use nginx to redirect it to the proper URL
+  //   const dataByIdResponse = await axios.get(`/api/values/${id}`);
+  //   setData(dataByIdResponse.data.map((row) => row));
+  //     // console.log(`\x1b[33m DEBUG : ---> ${data.map((value) => value.username)} \x1b[0m`)
+  // }, []);
 
   useEffect(() => {
-    getNumberById(state.id);
-  }, [getNumberById]);
+    // getNumberById(state.id);
+    const fetcData = async () => {
+      const data = await axios.get(`/api/values/${state.id}`);
+    setData(data.data.map((row) => row));
+  };
+  fetcData();
+  }, [state.id]);
 
   return (
     <>
       {data && data.map((value, idx) => (
         <div className="header-title" key={idx}>
-          <h1>
-            Details: <span>{value.username}</span>
-          </h1>
+          <h3>
+            Hello, <span>{value.username}</span>
+          </h3>
           <div>Other details</div>
           {value.number}
         </div>
--- client/src/components/screens/main/MainComponent.js
+++ client/src/components/screens/main/MainComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useCallback, useState, useEffect } from "react";
 import axios from "axios";
 import "./MainComponent.css";
-import { NavLink, Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const MainComponent = () => {
   const [values, setValues] = useState([]);
